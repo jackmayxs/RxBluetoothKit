081e6d19fb88546a2775f5eb4672ad7ad3ceab51
马永祥 <myx@godox.com>
2024-12-18 14:41:19 +0800

    RxSwift version 6.8.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "RxBluetoothKit", targets: ["RxBluetoothKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "8.0.0"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.8.0"))
     ],
     targets: [
         .target(
